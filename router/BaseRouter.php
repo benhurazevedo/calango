6acf4222d25ad0376457ea643b192672b7678df4
benhur alencar azevedo <benhur.azevedo@hotmail.com>
2018-03-13 11:21:24 -0300

    0.4 version redirect function improved

--- router/BaseRouter.php
+++ router/BaseRouter.php
@@ -1,108 +1,108 @@
-<?php 
+<?php
 namespace router;
 
 use Exception;
 
 class BaseRouter{
-	private $controller;
-	private $action;
-	public function loadAction(){
-		$controllerDeclared = isset($_GET["controller"]);
-		$actionDeclared = isset($_GET["action"]);
-		if($controllerDeclared && $actionDeclared){
-			try {
-				$class_exists = class_exists('controllers\\'.$_GET["controller"], true);
-			}
-			catch(Exception $e) {
-				$this->notFoundRouteAction();
-			}
-			$controllerFullName = '\\controllers\\'.$_GET["controller"];
-			$controller = new $controllerFullName($this);
-			$actionName = $_GET["action"];
-			if(method_exists($controller,$actionName)){
-				$controller->$actionName();
-			}
-			else{
-				$this->notFoundRouteAction();
-			}
-		}
-		else
-		{
-		    if(defined('DEFAULT_CONTROLLER') && defined('DEFAULT_ACTION'))
-		    {
-		        $this->responseRedirect(DEFAULT_CONTROLLER,DEFAULT_ACTION);
-		    }
-		    $this->notFoundRouteAction();
-		}
-	}
-	function __construct(){
-		$this->loadApplicationFilter($this);
-		$this->loadControllerFilter($this);
-		$this->loadActionFilter($this);
-		$this->loadAction();
-	}
-	/*
-		you can overload notFoundRouteAction to atempts to your needs
-	*/
-	public function notFoundRouteAction(){
-		echo "Route not found.";
-	}
-	
-	private function loadFilter($filterClassName=null)
-	{
-		if($filterClassName==null)
-		{
-			return;
-		}
-		try 
-		{
-			$class_exists = class_exists($filterClassName,true);
-		}
-		catch(Exception $e)
-		{
-			return;
-		}
-		if($class_exists)
-		{
-			$className = '\\'. $filterClassName;
-			new $className();
-		}
-		
-	}
-	public function loadApplicationFilter()
-	{
-		$this->loadFilter('filters\\ApplicationFilter');
-	}
-	public function loadControllerFilter()
-	{
-		if(isset($_GET["controller"]))
-		{
-			$this->loadFilter('filter\\' . $_GET["controller"] . '\\ControllerFilter');
-		}
-	}
-	public function loadActionFilter()
-	{
-		if(isset($_GET["controller"]) && isset($_GET["action"]))
-		{
-			if(isset($_GET["controller"]))
-			{
-				$this->loadFilter('filter\\' . $_GET["controller"] . '\\'. $_GET["action"] .'Filter'); 
-			}
-		}
-	}
-	public function responseRedirect($controller=null,$action=null,$id=null)
-	{
-	    if($controller==null && $action==null)
-	    {
-	        exit("Controller or action not defined.");
-	    }
-	    if($id == null)
-	    {
-	        header("Location:../".$controller."/".$action);
-	        return;
-	    }
-	    header("Location:../".$controller."/".$action."/".$id);
-	    return;
-	}
+    private $controller;
+    private $action;
+    public function loadAction(){
+        $controllerDeclared = isset($_GET["controller"]);
+        $actionDeclared = isset($_GET["action"]);
+        if($controllerDeclared && $actionDeclared){
+            try {
+                $class_exists = class_exists('controllers\\'.$_GET["controller"], true);
+            }
+            catch(Exception $e) {
+                $this->notFoundRouteAction();
+            }
+            $controllerFullName = '\\controllers\\'.$_GET["controller"];
+            $controller = new $controllerFullName($this);
+            $actionName = $_GET["action"];
+            if(method_exists($controller,$actionName)){
+                $controller->$actionName();
+            }
+            else{
+                $this->notFoundRouteAction();
+            }
+        }
+        else
+        {
+            if(defined('DEFAULT_CONTROLLER') && defined('DEFAULT_ACTION'))
+            {
+                $this->responseRedirect(DEFAULT_CONTROLLER,DEFAULT_ACTION);
+            }
+            $this->notFoundRouteAction();
+        }
+    }
+    function __construct(){
+        $this->loadApplicationFilter($this);
+        $this->loadControllerFilter($this);
+        $this->loadActionFilter($this);
+        $this->loadAction();
+    }
+    /*
+     you can overload notFoundRouteAction to atempts to your needs
+     */
+    public function notFoundRouteAction(){
+        echo "Route not found.";
+    }
+    
+    private function loadFilter($filterClassName=null)
+    {
+        if($filterClassName==null)
+        {
+            return;
+        }
+        try
+        {
+            $class_exists = class_exists($filterClassName,true);
+        }
+        catch(Exception $e)
+        {
+            return;
+        }
+        if($class_exists)
+        {
+            $className = '\\'. $filterClassName;
+            new $className();
+        }
+        
+    }
+    public function loadApplicationFilter()
+    {
+        $this->loadFilter('filters\\ApplicationFilter');
+    }
+    public function loadControllerFilter()
+    {
+        if(isset($_GET["controller"]))
+        {
+            $this->loadFilter('filter\\' . $_GET["controller"] . '\\ControllerFilter');
+        }
+    }
+    public function loadActionFilter()
+    {
+        if(isset($_GET["controller"]) && isset($_GET["action"]))
+        {
+            if(isset($_GET["controller"]))
+            {
+                $this->loadFilter('filter\\' . $_GET["controller"] . '\\'. $_GET["action"] .'Filter');
+            }
+        }
+    }
+    public function responseRedirect($controller=null,$action=null,$id=null)
+    {
+        if($controller==null && $action==null)
+        {
+            exit("Controller or action not defined.");
+        }
+        if($id == null)
+        {
+            header("Location:".PATH."/".$controller."/".$action);
+            return;
+        }
+        header("Location:". PATH. "/" .$controller."/".$action."/".$id);
+        return;
+    }
 }
 ?>
--- services/htmlHelper/HtmlHelperService.php
+++ services/htmlHelper/HtmlHelperService.php
@@ -1,99 +1,102 @@
 <?php
 namespace services\htmlHelper;
 class HtmlHelperService{
-	function __construct(){
-		if(isset($_SESSION["initialized"]))
-		{
-			if($_SESSION["initialized"])
-			{
-				return;
-			}
-		}
-		session_start();
-		$_SESSION["initialized"] = true;
-	}
-	public function actionUrl($controllerName, $actionName, $routeParams="")
-	{
-		if($routeParams=="")
-		{
-			#return "../../index.php?controller=".$controllerName."&action=".$actionName;
-			return $this->generateBaseURL().$controllerName."/".$actionName;
-		}
-		else
-		{
-			#return "index.php?controller=".$controllerName."&action=".$actionName."&".$routeParams;
-			return $this->generateBaseURL().$controllerName."/".$actionName."/".$routeParams; 	
-		}
-	}
-	public function resourceUrl($basePath)
-	{
-		return $this->generateBaseURL() . $basePath;
-	}
-	private function generateBaseURL()
-	{
-		if(isset($_GET["id"]) && isset($_GET["action"]) && isset($_GET["controller"]))
-		{
-			return "../../";
-		}
-		else if(isset($_GET["action"]) && isset($_GET["controller"]))
-		{
-			return "../";
-		}
-		else 
-		{
-			return "./";
-		}
-	}
-	public function setTempData($name=NULL, $value=NULL)
-	{
-		if($name == NULL || $value == NULL)
-		{
-			return;
-		}
-		$key = $name."TempData";
-		$_SESSION[$key] = $value;
-	}
-	public function getTempData($name=NULL)
-	{
-		if($name == NULL)
-		{
-			return;
-		}
-		$key = $name."TempData";
-		$value = isset($_SESSION[$key])?$_SESSION[$key]:null;
-		unset($_SESSION[$key]);
-		return $value;
-	}
-	public function setFormToken()
-	{
-	    $token = rand();
-	    if(!isset($_SESSION['formToken']))
-	    {
-	        $_SESSION['formToken'] = array();
-	    }
-	    array_push($_SESSION['formToken'], $token);
-	    return "<input type='hidden' name='formToken' value='$token'>";
-	}
-	public function validateFormToken()
-	{
-	    if(!isset($_SESSION['formToken']))
-	    {
-	        return false;
-	    }
-	    if(!is_array($_SESSION['formToken']))
-	    {
-	        return false;
-	    }
-	    if(!isset($_POST['formToken']))
-	    {
-	        return false;
-	    }
-	    if(!$formTokenIndex = array_search($_POST['formToken'], $_SESSION['formToken']))
-	    {
-	        return false;
-	    }
-	    unset($_SESSION['formToken'][$formTokenIndex]);
-	    return true;
-	}
+    function __construct(){
+        if(isset($_SESSION["initialized"]))
+        {
+            if($_SESSION["initialized"])
+            {
+                return;
+            }
+        }
+        session_start();
+        $_SESSION["initialized"] = true;
+    }
+    public function actionUrl($controllerName, $actionName, $routeParams="")
+    {
+        if($routeParams=="")
+        {
+            #return "../../index.php?controller=".$controllerName."&action=".$actionName;
+            return $this->generateBaseURL().$controllerName."/".$actionName;
+        }
+        else
+        {
+            #return "index.php?controller=".$controllerName."&action=".$actionName."&".$routeParams;
+            return $this->generateBaseURL().$controllerName."/".$actionName."/".$routeParams;
+        }
+    }
+    public function resourceUrl($basePath)
+    {
+        return $this->generateBaseURL() . $basePath;
+    }
+    private function generateBaseURL()
+    {
+        /*
+         if(isset($_GET["id"]) && isset($_GET["action"]) && isset($_GET["controller"]))
+         {
+         return "../../";
+         }
+         else if(isset($_GET["action"]) && isset($_GET["controller"]))
+         {
+         return "../";
+         }
+         else
+         {
+         return "./";
+         }
+         */
+        return defined("PATH")?PATH:"";
+    }
+    public function setTempData($name=NULL, $value=NULL)
+    {
+        if($name == NULL || $value == NULL)
+        {
+            return;
+        }
+        $key = $name."TempData";
+        $_SESSION[$key] = $value;
+    }
+    public function getTempData($name=NULL)
+    {
+        if($name == NULL)
+        {
+            return;
+        }
+        $key = $name."TempData";
+        $value = isset($_SESSION[$key])?$_SESSION[$key]:null;
+        unset($_SESSION[$key]);
+        return $value;
+    }
+    public function setFormToken()
+    {
+        $token = rand();
+        if(!isset($_SESSION['formToken']))
+        {
+            $_SESSION['formToken'] = array();
+        }
+        array_push($_SESSION['formToken'], $token);
+        return "<input type='hidden' name='formToken' value='$token'>";
+    }
+    public function validateFormToken()
+    {
+        if(!isset($_SESSION['formToken']))
+        {
+            return false;
+        }
+        if(!is_array($_SESSION['formToken']))
+        {
+            return false;
+        }
+        if(!isset($_POST['formToken']))
+        {
+            return false;
+        }
+        if(!$formTokenIndex = array_search($_POST['formToken'], $_SESSION['formToken']))
+        {
+            return false;
+        }
+        unset($_SESSION['formToken'][$formTokenIndex]);
+        return true;
+    }
 }
 ?>
